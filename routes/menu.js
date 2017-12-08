2c01d14d0aad2003156937341c933b0969d93498
bwyfed <36172659@qq.com>
2017-12-08 17:40:30 +0800

    对于CORS请求，设置请求的Origin为允许访问的域。

--- routes/menu.js
+++ routes/menu.js
@@ -7,6 +7,12 @@ let headerData = require('../mock/header-data.json');
 
 // 获取头部部分菜单数据
 router.get('/header',(req, res, next)=>{
+    let origin = req.get('Origin');
+    console.log(origin);
+    //设置允许的跨域请求
+    res.set({
+        'Access-Control-Allow-Origin': origin
+    });
     res.json({
         "status": 0,
         "msg": "header data success",
