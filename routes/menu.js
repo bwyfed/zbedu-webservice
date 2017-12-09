2e0dc3523c6d0f42796aec9de09cca45036eb0ba
bwyfed <36172659@qq.com>
2017-12-09 14:39:40 +0800

    增加首页菜单数据的路由。

--- routes/menu.js
+++ routes/menu.js
@@ -4,6 +4,7 @@
 let express = require('express');
 let router = express.Router();
 let headerData = require('../mock/header-data.json');
+let indexData = require('../mock/index-main.json');
 
 // 获取头部部分菜单数据
 router.get('/header',(req, res, next)=>{
@@ -11,6 +12,14 @@ router.get('/header',(req, res, next)=>{
         "status": 0,
         "msg": "header data success",
         "result": headerData.result
+    });
+});
+// 获取首页的菜单数据
+router.get('/index',(req,res,next)=>{
+    res.json({
+        "status": 0,
+        "msg": "index data success",
+        "result": indexData
     });
 });
 
