116bccab85853a6d45f0cde4a16fa657089379d8
bwyfed <36172659@qq.com>
2017-12-21 18:01:11 +0800

    增加简单的注册和登录的表单视图；增加会话相关和存储相关的插件，并在入口文件中使用session中间件；实现注册和登录的前后端基本功能。

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');//session中间件
+var mongoStore = require('connect-mongo')(session);
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 let menu = require('./routes/menu');
-
+//数据库连接字符串
+let dbUrl = 'mongodb://localhost/zbedu';
 var app = express();
+
+mongoose.connect(dbUrl,{useMongoClient: true});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,13 +25,34 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({
+    secret: 'zbedu',
+    resave: false,
+    saveUninitialized: true,
+    store: new mongoStore({
+        url: dbUrl,
+        collections: 'sessions'
+    })
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
 app.use('/menu', menu);
+//注册用户
+app.post('/user/register',(req, res)=>{
+    let _user = req.body.user;
+    res.set('Content-Type','text/plain');
+    res.end('注册成功');
+});
+//用户登录
+app.post('/user/login',(req, res)=>{
+    let _user = req.body.user;
+    res.set('Content-Type','text/plain');
+    res.end('登录成功');
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
